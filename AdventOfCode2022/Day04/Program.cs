8a0c6a72667dea52241b0170ba295f98097d8fb6
Jacob Postema <jacob.postema@deluxe.com>
2022-12-15 09:38:01 -0500

    start Day04

---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,21 @@
+﻿var lines = System.IO.File.ReadAllLines(@"/Users/t463971/code/advent_of_code/AdventOfCode2022/Day04/input.txt");
+
+Console.WriteLine("########## Day 2 2022 ##########");
+Console.WriteLine($"Part one solution: {SolvePartOne(lines)}");
+Console.WriteLine($"Part two solution: {SolvePartTwo(lines)}");
+Console.WriteLine("################################");
+
+
+static int SolvePartOne(string[] rucksackContents)
+{
+    var sumOfIncorrectItems = 0;
+
+    return sumOfIncorrectItems;
+}
+
+static int SolvePartTwo(string[] rucksackContents)
+{
+    var sumOfBadges = 0;
+
+    return sumOfBadges;
+}
